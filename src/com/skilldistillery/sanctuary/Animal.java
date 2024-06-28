e8f7186dc2509d788f6d5e774255ed4a0fbb0675
Rob Roselius <roselius@skilldistillery.com>
2024-06-28 17:22:39 -0600

    Complete Animal class

--- src/com/skilldistillery/sanctuary/Animal.java
+++ src/com/skilldistillery/sanctuary/Animal.java
@@ -10,5 +10,14 @@ public class Animal {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public void makeNoise() {
+		System.out.println("???");
+	}
+	
+	public void eat(int amount) {
+		System.out.println("I am eating " + amount + " foods.");
+		
+	}
 
 }
