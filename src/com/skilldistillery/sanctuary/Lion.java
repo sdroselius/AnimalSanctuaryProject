06e250e7248e662314b836cc43d9e1ea594d7b4c
Rob Roselius <roselius@skilldistillery.com>
2024-06-28 17:29:03 -0600

    Complete Lion class

--- src/com/skilldistillery/sanctuary/Lion.java
+++ src/com/skilldistillery/sanctuary/Lion.java
@@ -1,5 +1,17 @@
 package com.skilldistillery.sanctuary;
 
 public class Lion extends Animal {
+	private static String noise = "RAWWRR!!";
 
+	@Override
+	public void makeNoise() {
+		System.out.println(noise);
+	}
+
+	@Override
+	public void eat(int amount) {
+		makeNoise();
+		System.out.println("Im tearing into " + amount + " meats!");
+	}
+	
 }
