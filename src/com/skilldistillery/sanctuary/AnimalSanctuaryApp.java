40868493dd97aeb23beb2a90602eb8ba69e3afa3
Rob Roselius <roselius@skilldistillery.com>
2024-06-28 18:07:12 -0600

    Main app loop

--- src/com/skilldistillery/sanctuary/AnimalSanctuaryApp.java
+++ src/com/skilldistillery/sanctuary/AnimalSanctuaryApp.java
@@ -6,7 +6,7 @@ public class AnimalSanctuaryApp {
 
 	private Sanctuary sanctuary;
 	private Scanner kb = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
@@ -16,10 +16,43 @@ public class AnimalSanctuaryApp {
 
 	private void launchApp() {
 		printMainMenu();
-	
+		boolean keepGoing = true;
+
+		do {
+			printMainMenu();
+			int choice = kb.nextInt();
+			kb.nextLine();
+			switch (choice) {
+				case 1:
+					if (sanctuary != null) {
+						sanctuary.listAnimals();
+					}
+					break;
+				case 2:
+					addAnimalToSanctuary();
+					break;
+				case 3:
+					if (sanctuary != null) {
+						sanctuary.startAttendantRounds();
+					}
+					break;
+				case 4: 
+					keepGoing = false;
+					break;
+				default:
+					System.out.println("Invalid choice: " + choice);
+					break;
+			}
+		} while (keepGoing);
+
 		kb.close();
 	}
-	
+
+	private void addAnimalToSanctuary() {
+		// TODO Auto-generated method stub
+		
+	}
+
 	private void printMainMenu() {
 		System.out.println("** Welcome to the Animal Sanctuary **");
 		System.out.println("1) List animals");
